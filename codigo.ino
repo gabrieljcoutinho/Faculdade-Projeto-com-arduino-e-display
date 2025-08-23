7b619c65bcb0c2d8f507df1165b6ce4adc56ef6e
Gabriel <gabrieljcoutinho@gmail.com>
2025-08-23 12:31:53 -0300

    mudano texto a cada 5 segundos

--- codigo.ino
+++ codigo.ino
@@ -1,5 +1,3 @@
-
-// include the library code:
 #include <LiquidCrystal.h>
 
 // Ligações do Arduino
@@ -7,16 +5,21 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  
-  lcd.begin(16, 2); // Aqui informo que são 16 caracteres e 2 linhas
-  lcd.setCursor(0, 0); // To colocando para exibir a frase na coluna 0 e linha 0 (opcional)
-  lcd.print("Projeto"); // Escrevo "Arduino"
-  lcd.setCursor(0, 1); // E agora to colocando para exibir a frase na coluna 0 e linha 1
-  lcd.print("funcionando "); // E escrevo "Display LCD"
+  lcd.begin(16, 2);
 }
 
-
 void loop() {
-  
-  delay(1000);
+  // Primeira mensagem
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Projeto");
+  lcd.setCursor(0, 1);
+  lcd.print("Projeto funcionando");
+  delay(5000);
+
+  // Segunda mensagem
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Nova mensagem");
+  delay(5000);
 }
